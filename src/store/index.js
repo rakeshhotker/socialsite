4d3a4006f235a80467817898d0621cc6b2f14516
raki <rakeshhotker@gmail.com>
2021-01-26 12:46:49 +0530

    added store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
-import { applyMiddleware, createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from '../reducers';
 import logger from 'redux-logger';
+import reducer from '../reducers';
+
 let store;
+
 export function configureStore() {
   store = createStore(reducer, applyMiddleware(thunk, logger));
+
   return store;
 }
