a5e2c8b1e25049cd3d2599071321ab56583bc606
raki <rakeshhotker@gmail.com>
2021-01-28 23:22:32 +0530

    login logout functionalities added

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,31 +1,86 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { startSingup, signup } from '../actions/auth';
 
-class SignUp extends Component {
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+      confirmPassword: '',
+    };
+  }
+
+  handleInputChange = (field, value) => {
+    this.setState({
+      [field]: value,
+    });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    const { email, password, confirmPassword, name } = this.state;
+
+    if (email && password && confirmPassword && name) {
+      this.props.dispatch(startSingup());
+      this.props.dispatch(signup(email, password, confirmPassword, name));
+    }
+  };
+
   render() {
+    const { inProgress, error } = this.props.auth;
     return (
       <form className="login-form">
-        <span className="login-signup-header">Sign Up</span>
+        <span className="login-signup-header"> Signup</span>
+        {error && <div className="alert error-dailog">{error}</div>}
         <div className="field">
-          <input type="text" placeholder="FirstName" required />
+          <input
+            placeholder="Name"
+            type="text"
+            required
+            onChange={(e) => this.handleInputChange('name', e.target.value)}
+          />
         </div>
         <div className="field">
-          <input type="text" placeholder="LastName" required />
+          <input
+            placeholder="Email"
+            type="email"
+            required
+            onChange={(e) => this.handleInputChange('email', e.target.value)}
+          />
         </div>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input
+            placeholder="Confirm password"
+            type="password"
+            required
+            onChange={(e) =>
+              this.handleInputChange('confirmPassword', e.target.value)
+            }
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Set Password" required />
+          <input
+            placeholder="Password"
+            type="password"
+            required
+            onChange={(e) => this.handleInputChange('password', e.target.value)}
+          />
         </div>
         <div className="field">
-          <input type="text" placeholder="Phone-Number" required />
-        </div>
-        <div className="field">
-          <button>SignUp</button>
+          <button onClick={this.onFormSubmit} disabled={inProgress}>
+            Signup
+          </button>
         </div>
       </form>
     );
   }
 }
 
-export default SignUp;
+const mapStateToProps = ({ auth }) => ({
+  auth,
+});
+
+export default connect(mapStateToProps)(Signup);
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,7 +1,12 @@
 import {
+  AUTHENTICATE_USER,
   LOGIN_FAILED,
   LOGIN_START,
   LOGIN_SUCCESS,
+  LOG_OUT,
+  SIGNUP_FAILED,
+  SIGNUP_START,
+  SIGNUP_SUCCESS,
 } from '../actions/actionTypes';
 
 const initialAuthState = {
@@ -13,11 +18,13 @@ const initialAuthState = {
 export default function auth(state = initialAuthState, action) {
   switch (action.type) {
     case LOGIN_START:
+    case SIGNUP_START:
       return {
         ...state,
         inProgress: true,
       };
     case LOGIN_SUCCESS:
+    case SIGNUP_SUCCESS:
       return {
         ...state,
         user: action.user,
@@ -26,10 +33,23 @@ export default function auth(state = initialAuthState, action) {
         error: null,
       };
     case LOGIN_FAILED:
+    case SIGNUP_FAILED:
       return {
         ...state,
         inProgress: false,
         error: action.error,
+      };
+    case AUTHENTICATE_USER:
+      return {
+        ...state,
+        user: action.user,
+        isLoggedin: true,
+      };
+    case LOG_OUT:
+      return {
+        ...state,
+        user: {},
+        isLoggedin: false,
       };
     default:
       return state;
